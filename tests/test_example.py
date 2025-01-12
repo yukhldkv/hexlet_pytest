2d32f6f7dc5f191bcb0b49bf05aadacb276cbde3
Yura <yukhldkv@gmail.com>
2025-01-11 19:49:12 -0800

    test stack

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 from hexlet_pytest.example import reverse
 
 
@@ -7,3 +10,27 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert stack != []
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
